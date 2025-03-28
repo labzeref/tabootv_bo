5c62f18e39778a4d7332ba26b1743d6e75f33a77
Abdul Haseeb Khan <abdulhaseebkhan407@gmail.com>
2025-03-29 02:14:15 +0500

    Update app_container_script.sh

--- scripts/app_container_script.sh
+++ scripts/app_container_script.sh
@@ -12,6 +12,13 @@ else
     composer install
 fi
 
+# Cache Stuff
+echo "-----------Cache Stuff-----------"
+php artisan route:cache
+php artisan config:cache
+php artisan view:cache
+php artisan event:cache
+
 # Running services
 echo "-----------Running services-----------"
 php artisan ser --host 0.0.0.0 --port 8009 & php artisan reverb:start --port=6003 --host=0.0.0.0
